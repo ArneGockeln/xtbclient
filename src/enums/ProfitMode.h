389de0c6185d0089b9af2efdb913726f6da3d52c
Arne Gockeln <arne@gockeln.com>
2017-11-02 21:18:08 +0100

    fix: license header

--- src/enums/ProfitMode.h
+++ src/enums/ProfitMode.h
@@ -1,8 +1,19 @@
-/*
- * ProfitMode.h
- * SymbolRecord
- *  Created on: 10.06.2017
- *      Author: arnegockeln
+/*!
+ *  Copyright(c)2017, Arne Gockeln
+ *  http://www.arnegockeln.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef SRC_XTB_RECORD_PROFITMODE_H_
